ef397a3241c3d8a42a41952d695c78350d4cb43b
Zekizanian <andre_rubbo@hotmail.com>
2018-11-05 22:24:47 -0200

    Slider menu mobile

--- challenge-one/arquivos/default.js
+++ challenge-one/arquivos/default.js
@@ -1,29 +1,27 @@
 var products;
 var ingredientLi;
+var i;
 
-function setLightbox(dataId,src , ingredientLi) {
+function setLightbox(dataId, src, ingredientLi) {
     var potion = products.potions[dataId];
-    $('.lightbox img.potion-img').attr('src', src)
+    $('.lightbox img.potion-img').attr('src', src);
     $('.lightbox .potion-title').html(potion.name);
     $('.lightbox .effect span').html(potion.effect);
     $('.lightbox .price span').html('$' + potion.price);
-    for (var i = 0; i < potion.ingredients.length; i++) {
-        $('.lightbox .ingredients ul .ingredient.clean').html('<span>' + potion.ingredients[i] + "</span>").removeClass('clean')
-        if(i < potion.ingredients.length -1) {
-            $('.lightbox .ingredients ul').append(ingredientLi)
+    for (i = 0; i < potion.ingredients.length; i++) {
+        $('.lightbox .ingredients ul .ingredient.clean').html('<span>' + potion.ingredients[i] + "</span>").removeClass('clean');
+        if (i < potion.ingredients.length - 1) {
+            $('.lightbox .ingredients ul').append(ingredientLi);
         }
     }
-
-    $('.lightbox, #modal ').css('display', 'block')
-    
+    $('.lightbox, #modal ').css('display', 'block');
 }
 
 function requireProducts(spot) {
-    $.ajax ({
-        'url':'https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json'
-    }).done(function(data){
+    $.ajax({
+        'url' : 'https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json'
+    }).done(function (data) {
         products = data;
-        
         for (property in products) {
             $('.shelf-title').html(property);
         }
@@ -34,7 +32,7 @@ function requireProducts(spot) {
             $('.spot.clean:last-child .price').text('$' + key.price);
     
             $('.spot.clean:last-child').removeClass('clean');
-            if(index == 6){
+            if (index == 6){
                 return false;
             }
             $('#recebe-potions').append(spot);
@@ -44,17 +42,32 @@ function requireProducts(spot) {
 
 $(document).ready(function(){
     var spot = $('.spot').parent().html();
-    var ingredientFrame =$('.lighbox ul').html()
-    requireProducts(spot)
-    console.log($('.spot'))
-    
+    var ingredientFrame =$('.lighbox ul').html();
+    requireProducts(spot);
     $('.lightbox .close, #modal ').click(function(){
-        $('.lightbox, #modal ').css('display', 'none')
+        $('.lightbox, #modal ').css('display', 'none');
+        $('.menu-container.mobile-menu').animate({width:'0px'},500);
     })
-    ingredientLi = $('.lightbox .ingredients ul').html()
+    ingredientLi = $('.lightbox .ingredients ul').html();
+
+    $('.menu-container .menu').prepend($('.header-search').clone())
+    //abre menu mobile
+    $('.mobile-menu-icon').on('click', function(){
+        if(window.innerWidth < 768) {
+            if ( $('.menu-container .menu .header-search').length==0) {
+                $('.menu-container .menu').prepend($('.header-search').clone())
+            }
+            $('.menu-container').addClass('mobile-menu')
+            $('.menu-container.mobile-menu').animate({width:'315px'},500);
+            $('#modal').show()
+        }else{
+            $('.menu-container .menu').removeClass('mobile-menu')
+        }
+    });
+    $('fecha-menu-mobile')
 })
 $(document).on('click','.spot',function (){
-    $('.lightbox .ingredients ul').html(ingredientLi)
+    $('.lightbox .ingredients ul').html(ingredientLi);
     setLightbox($(this).attr('data-id'), $(this).find('img').attr('src'),ingredientLi);
 })
 
